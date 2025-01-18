61d7711dfc4da46fecf614db4fd6ebb5974a324d
Shwetabh <webwritat@gmail.com>
2025-01-19 02:14:19 +0530

    Initial commit project 'Isometric'

---
+++ portfolio.py
@@ -0,0 +1,16 @@
+import os
+from flask import Flask
+from routes.main import main
+from routes.about import about
+
+app = Flask(__name__)
+app.secret_key = 'dgirgr8489yg90rjg0HG(U#UU(Ggrggj90rge5'
+
+# ------------------------------------- BLUEPRINTS ---------------------------------- #
+
+app.register_blueprint(main, url_prefix='/')
+app.register_blueprint(about, url_prefix='/about')
+
+if __name__ == '__main__':
+    app.run(host='0,0,0,0', port=5000)
+    # app.run(debug=True)
---
+++ routes/main.py
@@ -0,0 +1,40 @@
+from flask import Blueprint, render_template
+from PIL import Image
+import os
+import operations as o
+from datetime import date
+current_date = date.today()
+current_year = current_date.year
+
+
+main = Blueprint('main', __name__, static_folder='static', template_folder='templates')
+
+
+@main.route('/')
+def home():
+    # portraits = []
+    # landscapes = []
+    all_images = []
+
+    path = 'static/images/Illustrations/'
+    for f in os.listdir(path):
+        full_path = path + f
+        all_images.append(full_path)
+    # details = o.image_details(path)
+    # for d in details:
+    #     orientation = d[3]
+    #     if orientation == 'portrait':
+    #         portraits.append(d[0])
+    #     else:
+    #         landscapes.append(d[0])
+    #
+    # for i in portraits:
+    #     all_images.append(i)
+    #
+    # for j in landscapes:
+    #     all_images.append(j)
+
+    # details.sort()
+    # print(details)
+
+    return render_template('index.html', images=all_images, current_year=current_year)
